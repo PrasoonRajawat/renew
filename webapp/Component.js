42846b893071ccedf4243070e22d502c1b3c218c
basimcap <basim@capriconics.com>
2023-05-16 01:50:10 +0530

    Logo issue fixed

--- webapp/Component.js
+++ webapp/Component.js
@@ -3,10 +3,10 @@
  */
 
 sap.ui.define([
-        "sap/ui/core/UIComponent",
-        "sap/ui/Device",
-        "com/ibscms/demo/model/models"
-    ],
+    "sap/ui/core/UIComponent",
+    "sap/ui/Device",
+    "com/ibscms/demo/model/models"
+],
     function (UIComponent, Device, models) {
         "use strict";
 
@@ -29,6 +29,14 @@ sap.ui.define([
 
                 // set the device model
                 this.setModel(models.createDeviceModel(), "device");
+
+                var oRootPath = jQuery.sap.getModulePath("com.ibscms.demo"); // your resource root
+
+                var oImageModel = new sap.ui.model.json.JSONModel({
+                    path: oRootPath,
+                });
+
+                this.setModel(oImageModel, "imageModel");
             }
         });
     }
